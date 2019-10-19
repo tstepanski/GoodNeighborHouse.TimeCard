0d3ee1d4229b2980a7136e010a25887cf0784ad6
Larry Wildey <larry.wildey@centricconsulting.com>
2019-10-19 09:48:56 -0400

    initial vm and controller creation

---
+++ GoodNeighborHouse.TimeCard.Web/Controllers/TimeCardController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodNeighborHouse.TimeCard.Web.Controllers
+{
+    public class TimeCardController : Controller
+    {
+        // GET: TimeCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: TimeCard/AddPunch
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPunch(Guid userID, string punchType)
+        {
+            try
+            {
+                // TODO: Add punch to db
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TimeCard/Edit/5
+        public ActionResult Edit(Guid userID)
+        {
+            return View();
+        }
+
+        // POST: TimeCard/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Guid userID, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TimeCard/Delete/5
+        public ActionResult Delete(Guid userID)
+        {
+            return View();
+        }
+
+        // POST: TimeCard/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Guid userID, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GoodNeighborHouse.TimeCard.Web/Models/TimeCardViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodNeighborHouse.TimeCard.Web.Models
+{
+    public class TimeCardViewModel
+    {
+        public Guid ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        //public List<Punch> Punches { get; set; }
+    }
+}
