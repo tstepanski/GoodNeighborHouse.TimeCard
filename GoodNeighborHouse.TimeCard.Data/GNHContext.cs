00123cf1ab4014f73f5dfb5cf3c1e2fae468250e
charlie.retzler <charlie.retzler@ingagepartners.com>
2019-10-19 08:30:09 -0400

    Adding Organizations and Departments

--- GoodNeighborHouse.TimeCard.Data/GNHContext.cs
+++ GoodNeighborHouse.TimeCard.Data/GNHContext.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace GoodNeighborHouse.TimeCard.Data
 {
     public class GNHContext : DbContext
 	{
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
         public DbSet<Volunteer> Volunteers { get; set; }
 
 		public GNHContext(DbContextOptions options) : base(options)
@@ -30,6 +34,14 @@ namespace GoodNeighborHouse.TimeCard.Data
             {
                 property.SetDefaultValueSql(@"NEWSEQUENTIALID()");
             }
+
+            modelBuilder.Entity<Department>()
+                .HasData(new List<Department>
+                {
+                    new Department() {ID = new Guid("00000000-0000-0000-0000-000000000001"), Name = "Dental"},
+                    new Department() {ID = new Guid("00000000-0000-0000-0000-000000000002"), Name = "Human Services"},
+                    new Department() {ID = new Guid("00000000-0000-0000-0000-000000000003"), Name = "Medical"}
+                });
         }
     }
 }
