ea632c11c33e8954a4acc0be6c796ad1a18716a6
Timothy M. Stepanski <timothy.stepanski@kroger.com>
2019-10-18 22:14:43 -0400

    cleaned up entity

--- GoodNeighborHouse.TimeCard.Data/Entities/Punch.cs
+++ GoodNeighborHouse.TimeCard.Data/Entities/Punch.cs
@@ -2,28 +2,32 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-[Table("Punch")]
-public class Punch {
-    public int PunchId { get; set; }
+namespace GoodNeighborHouse.TimeCard.Data.Entities
+{
+    [Table(@"Punch")]
+    public class Punch {
+        [Key, Column(@"ID", TypeName = @"INT"), Required]
+        public int Id { get; set; }
 
-    [Required]
-    public int VolunteerId { get; set; }
+        [Column(@"VolunteerId", TypeName = @"INT"), Required]
+        public int VolunteerId { get; set; }
     
-    [Required]
-    public DateTime PunchTime { get; set; }
+        [Column(@"PunchTime", TypeName = @"DATETIME"), Required]
+        public DateTime PunchTime { get; set; }
     
-    [Required]
-    public int IsDeleted { get; set; }
+        [Column(@"IsDeleted", TypeName = @"BIT"), Required]
+        public bool IsDeleted { get; set; }
     
-    [Required]
-    public int CreatedBy { get; set; }
+        [Column(@"CreatedBy", TypeName = @"INT"), Required]
+        public int CreatedBy { get; set; }
     
-    [Required]
-    public DateTime CreatedAt { get; set; }
+        [Column(@"CreatedAt", TypeName = @"DATETIME"), Required]
+        public DateTime CreatedAt { get; set; }
     
-    [Required]
-    public int LastUpdatedBy { get; set; }
+        [Column(@"LastUpdatedBy", TypeName = @"INT"), Required]
+        public int LastUpdatedBy { get; set; }
     
-    [Required]
-    public DateTime UpdatedAt { get; set; }
+        [Column(@"UpdatedAt", TypeName = @"DATETIME"), Required]
+        public DateTime UpdatedAt { get; set; }
+    }
 }
