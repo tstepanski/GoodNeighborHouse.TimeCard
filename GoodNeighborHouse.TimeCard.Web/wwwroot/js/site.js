2e09811cca116d19d07d4e76960c30c5c37076e5
Timothy M. Stepanski <timothy.stepanski@kroger.com>
2019-10-19 21:12:00 -0400

    added start of api connector

--- GoodNeighborHouse.TimeCard.Web/wwwroot/js/site.js
+++ GoodNeighborHouse.TimeCard.Web/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var GoodNeighborHouseApi = {
+    CreatePunch: function(volunteerId, isClockIn, forTimeInMilliseconds, callback) {
+        $.ajax({
+            type: "POST",
+            url: "/api/punch/create/" + volunteerId + "/" + isClockIn + "/" + forTime,
+            success: callback
+        });
+    };
+    
+    DeletePunch: function(id, callback) {
+        $.ajax({
+            type: "DELETE",
+            url: "/api/punch/" + id,
+            success: callback
+        });
+    };
+};
