216cf3158c8d49ddbb0e981a0944ca4fe28b3dad
charlie.retzler <charlie.retzler@ingagepartners.com>
2019-10-19 20:44:14 -0400

    Layout for main kiosk page

---
+++ GoodNeighborHouse.TimeCard.Web/Controllers/KioskController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoodNeighborHouse.TimeCard.Web.Controllers
+{
+    [AllowAnonymous]
+    public class KioskController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
