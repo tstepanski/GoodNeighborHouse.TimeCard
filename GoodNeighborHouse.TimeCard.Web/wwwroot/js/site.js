4fd0028eb1bcecf70e7de98ea1d1d01de6ddfeb2
Timothy M. Stepanski <timothy.stepanski@kroger.com>
2019-10-20 13:58:07 -0400

    fixed kiosk mode

--- GoodNeighborHouse.TimeCard.Web/wwwroot/js/site.js
+++ GoodNeighborHouse.TimeCard.Web/wwwroot/js/site.js
@@ -5,7 +5,31 @@
 
 window.GNH = (window.GNH || {});
 
+window.GNH.increaseValue = (window.GNH.increaseValue || function () {
+    var value = parseInt(document.getElementById('number').value);
+    value++;
+    document.getElementById('number').value = value;
+});
+
+window.GNH.decreaseValue = (window.GNH.decreaseValue || function () {
+    var value = parseInt(document.getElementById('number').value);
+    value < 2 ? value = 2 : '';
+    value--;
+    document.getElementById('number').value = value;
+});
+
 window.GNH.hideOnPunchInSelect = (window.GNH.hideOnPunchInSelect || function() {
     document.getElementById("punch-buttons").style.display = "none";
     document.getElementById("department-select").style.display = "block";
 });
+
+window.GNH.showQuantitySelect = (window.GNH.showQuantitySelect || function () {
+    document.getElementById("quantity-select").style.display = "none";
+    document.getElementById("department-select").style.display = "none";
+    document.getElementById("quantity-select").style.display = "block";
+});
+
+window.GNH.getQuantity = (window.GNH.getQuantity || function () {
+    return parseInt(document.getElementById('number').value);
+});
+
