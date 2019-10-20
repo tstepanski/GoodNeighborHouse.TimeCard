84eb9d2341b2b5806929739f9e89a4f3a91e4122
Timothy M. Stepanski <timothy.stepanski@kroger.com>
2019-10-19 21:50:05 -0400

    intermediate

--- GoodNeighborHouse.TimeCard.Web/Controllers/KioskController.cs
+++ GoodNeighborHouse.TimeCard.Web/Controllers/KioskController.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading;
+﻿using System;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using GoodNeighborHouse.TimeCard.Data.Entities;
 using GoodNeighborHouse.TimeCard.Data.Repositories;
 using GoodNeighborHouse.TimeCard.Data.UnitOfWork;
 using GoodNeighborHouse.TimeCard.General;
@@ -37,9 +40,72 @@ namespace GoodNeighborHouse.TimeCard.Web.Controllers
 			}
 		}
 
-		public Task<IActionResult> Punch()
+		[HttpPost(@"PunchIn/{volunteerId}/{departmentId}")]
+		public async Task<IActionResult> PunchIn([FromRoute] Guid volunteerId, [FromRoute] Guid departmentId,
+			[FromQuery] int? quantity, CancellationToken cancellationToken = default)
 		{
-			return null;
+			using (var unitOfWork = _unitOfWorkFactory.CreateReadWrite())
+			{
+				var userRepository = unitOfWork.GetRepository<IVolunteerRepository>();
+				var punchRepository = unitOfWork.GetRepository<IPunchRepository>();
+				var volunteer = await userRepository.GetAsync(volunteerId, cancellationToken);
+
+				quantity ??= 1;
+
+				if (volunteer.IsGroup && quantity > 1)
+				{
+					throw new ArgumentException(@"Non-group volunteers may not specify a quantity of greater than 1");
+				}
+
+				var entity = new Punch
+				{
+					Quantity = (int) quantity,
+					DepartmentId = departmentId,
+					Volunteer = volunteer,
+					CreatedAt = DateTime.Now,
+					CreatedBy = volunteer.Username,
+					IsDeleted = false,
+					PunchTime = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+					IsClockIn = true,
+					LastUpdatedBy = volunteer.Username
+				};
+
+				await punchRepository.AddAsync(entity, cancellationToken);
+
+				return Accepted();
+			}
+		}
+
+		[HttpPost(@"PunchOut/{volunteerId}")]
+		public async Task<IActionResult> PunchOut([FromRoute] Guid volunteerId,
+			CancellationToken cancellationToken = default)
+		{
+			using (var unitOfWork = _unitOfWorkFactory.CreateReadWrite())
+			{
+				var userRepository = unitOfWork.GetRepository<IVolunteerRepository>();
+				var punchRepository = unitOfWork.GetRepository<IPunchRepository>();
+				var volunteer = await userRepository.GetAsync(volunteerId, cancellationToken);
+				var departmentId = volunteer.DepartmentVolunteers.First().Department.Id;
+
+				var entity = new Punch
+				{
+					Quantity = 0,
+					DepartmentId = departmentId,
+					Volunteer = volunteer,
+					CreatedAt = DateTime.Now,
+					CreatedBy = volunteer.Username,
+					IsDeleted = false,
+					PunchTime = DateTime.Now,
+					UpdatedAt = DateTime.Now,
+					IsClockIn = false,
+					LastUpdatedBy = volunteer.Username
+				};
+
+				await punchRepository.AddAsync(entity, cancellationToken);
+
+				return Accepted();
+			}
 		}
 	}
 }
