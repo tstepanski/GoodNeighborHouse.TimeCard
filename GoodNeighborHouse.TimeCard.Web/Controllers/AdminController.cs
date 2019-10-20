a43527116e2ce5a9f69cb6054a6f2d37c0bf3a5b
Demis Harper <Demis Harper@DESKTOP-04Q8EB1>
2019-10-20 14:48:57 -0400

    Admin

---
+++ GoodNeighborHouse.TimeCard.Web/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using GoodNeighborHouse.TimeCard.Web.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GoodNeighborHouse.TimeCard.Data.UnitOfWork;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GoodNeighborHouse.TimeCard.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IUnitOfWorkFactory _unitOfWorkFactory;
+        public AdminController(IUnitOfWorkFactory unitOfWorkFactory) {
+            _unitOfWorkFactory = unitOfWorkFactory;
+        }
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet()]
+        public IActionResult Kiosk(CancellationToken cancellationToken = default)
+        {
+            return RedirectToAction("Index", "Kiosk");
+        }
+
+        [HttpGet()]
+        public IActionResult Admin(CancellationToken cancellationToken = default)
+        {
+            return View("Admin");
+        }
+
+    }
+}
